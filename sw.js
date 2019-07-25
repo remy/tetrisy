fbdf3c17fd902659d3f8626e90a0c370ba3535fb
Remy Sharp <remy@remysharp.com>
2019-07-25 22:31:04 +0100

    bump sw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'v6::static';
+const cacheName = 'v7::static';
 
 function updateStaticCache() {
   return caches.open(cacheName).then(cache => {

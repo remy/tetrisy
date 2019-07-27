150f556c753724a951b835df816287208a65c9d2
Remy Sharp <remy@remysharp.com>
2019-07-27 09:02:15 +0100

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'v11::static';
+const cacheName = 'v12::static';
 
 function updateStaticCache() {
   return caches.open(cacheName).then(cache => {
